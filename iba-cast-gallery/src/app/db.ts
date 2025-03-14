683d498107fd5137b5a67d60c3d25c76ed04f88b
arus <57102101+arus307@users.noreply.github.com>
2025-03-14 09:33:53 +0000

    DBファイル作成(キャスト部分だけ一旦登録)

---
+++ iba-cast-gallery/src/app/db.ts
@@ -0,0 +1,72 @@
+
+export const db: {
+    tweets: any[];
+    casts: Cast[];
+} =
+{
+    "tweets":[
+
+    ],
+    "casts":[
+        {
+            id:1,
+            name:"メノウ",
+            introduceTweetId:"1878744119239209193",
+            color:"#00ff00",
+        },
+        {
+            id:2,
+            name:"クジャク",
+            introduceTweetId:"1879091090134864220",
+            color:"#00ffff",
+        },
+        {
+            id:3,
+            name:"ルリ",
+            introduceTweetId:"1879453474661777549",
+            color:"#0000ff",
+        },
+        {
+            id:4,
+            name:"スズ",
+            introduceTweetId: "1879815861902647388",
+            color:"#ff0000"
+        },
+        {
+            id:5,
+            name:"シンジュ",
+            introduceTweetId:"1880193349136838830",
+            color:"#ffff00",
+        },
+        {
+            id:6,
+            name:"コハク",
+            introduceTweetId:"1880540647691587684",
+            color:"#ff00ff",
+        },
+        {
+            id:7,
+            name:"ユウレン",
+            introduceTweetId:"1880903030205517885",
+            color:"#ffff00",
+        },
+        {
+            id:8,
+            name:"ヒスイ",
+            introduceTweetId:"1881265418603786573",
+            color:"#ff00ff",
+        },
+        {
+            id:9,
+            name:"ホタル",
+            introduceTweetId:"1881627800299565408",
+            color:"#ff00ff",
+        },
+        {
+            id:10,
+            name:"ザクロ",
+            introduceTweetId:"1881990198072062114",
+            color:"#ff00ff",
+        }
+    ]
+}
