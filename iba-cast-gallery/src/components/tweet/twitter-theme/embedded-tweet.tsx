d09a40f3e7f1ec995558be19240e87c8ced0d8d6
arus <57102101+arus307@users.noreply.github.com>
2025-03-16 03:47:13 +0000

    ツイートの画像以外の表示切替を追加

--- iba-cast-gallery/src/components/tweet/twitter-theme/embedded-tweet.tsx
+++ iba-cast-gallery/src/components/tweet/twitter-theme/embedded-tweet.tsx
@@ -10,8 +10,8 @@ import { TweetActions } from './tweet-actions'
 import { TweetReplies } from './tweet-replies'
 import { QuotedTweet } from './quoted-tweet/index'
 import { enrichTweet } from '../utils'
-import { useMemo } from 'react'
-import {Stack} from '@mui/material'
+import { useMemo, useState } from 'react'
+import {Button, Stack} from '@mui/material'
 import CastChip from 'components/CastChip'
 
 type Props = {
@@ -23,23 +23,25 @@ type Props = {
 export const EmbeddedTweet = ({ tweet: t, components, taggedCasts }: Props) => {
   // useMemo does nothing for RSC but it helps when the component is used in the client (e.g by SWR)
   const tweet = useMemo(() => enrichTweet(t), [t])
+  const [displayTweet, setDisplayTweet] = useState<boolean>(false);
   return (
     <TweetContainer>
-      {/* <TweetHeader tweet={tweet} components={components} /> */}
-      {/* {tweet.in_reply_to_status_id_str && <TweetInReplyTo tweet={tweet} />} */}
-      {/* <TweetBody tweet={tweet} /> */}
+      {displayTweet && <TweetHeader tweet={tweet} components={components} />}
+      {displayTweet && tweet.in_reply_to_status_id_str && <TweetInReplyTo tweet={tweet} />}
+      {displayTweet && <TweetBody tweet={tweet} />}
       {tweet.mediaDetails?.length ? (
         <TweetMedia tweet={tweet} components={components} />
       ) : null}
-      {/* {tweet.quoted_tweet && <QuotedTweet tweet={tweet.quoted_tweet} />} */}
+      { displayTweet && tweet.quoted_tweet && <QuotedTweet tweet={tweet.quoted_tweet} />}
        <TweetInfo tweet={tweet} />
-      {/*<TweetActions tweet={tweet} />
-      <TweetReplies tweet={tweet} /> */}
-      <Stack direction='row' spacing={1} sx={{m:0.5}}>
+      {displayTweet && <TweetActions tweet={tweet} />}
+      {displayTweet && <TweetReplies tweet={tweet} /> }
+      <Stack direction='row' spacing={1} sx={{m:0.5}} alignItems={'center'}>
         {taggedCasts.map((cast) => (
           <CastChip key={cast.id} cast={cast} />
         ))}
       </Stack>
+      <Button size="small" variant="outlined" onClick={()=>setDisplayTweet(!displayTweet)}>ツイート全体を表示</Button>
     </TweetContainer>
   )
 }
