301823a00d5cb68c1d2fc9c6c270d2c9e8f9498b
arus <57102101+arus307@users.noreply.github.com>
2025-03-22 12:52:02 +0000

    キャスト一覧ページを仮実装

--- iba-cast-gallery/src/app/casts/page.tsx
+++ iba-cast-gallery/src/app/casts/page.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import getDb from 'getDb'; 
+import {Stack, Grid2 } from "@mui/material";
+import Tweets from "components/MediaTweetList";
+import { Tweet } from "components/tweet/swr";
 
-const CastsPage = () => {
+const CastsPage = async () => {
+
+    const db: JoinedDb = await getDb();
+
     return (
-        <div>
-            <h1>Casts Page</h1>
-            <p>Welcome to the Casts Page!</p>
-        </div>
+        <Grid2 container spacing={2} className="w-full">
+        {db.casts.map((cast) => (
+            <Grid2 key={cast.id} size={{xs:12, md:6, lg:4, xl:3}}>
+                <Tweet id={cast.introduceTweetId} taggedCasts={[cast]}/>
+            </Grid2>
+        ))}
+    </Grid2>
     );
 };
 
