52dd8e13a9365e9e1b586c449693527cc59e56f4
arus <57102101+arus307@users.noreply.github.com>
2025-04-14 12:39:26 +0000

    ビルド設定を変更(daoパッケージが含まれるように)

--- app/iba-cast-gallery/next.config.ts
+++ app/iba-cast-gallery/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ['@iba-cast-gallery/dao']
 };
 
 export default nextConfig;
