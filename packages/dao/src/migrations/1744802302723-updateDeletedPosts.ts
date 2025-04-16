5d1dc8b87780af6bedb87a91613cf2fe696c0cc2
arus <57102101+arus307@users.noreply.github.com>
2025-04-16 13:02:14 +0000

    削除されたツイートにフラグをセット

---
+++ packages/dao/src/migrations/1744802302723-updateDeletedPosts.ts
@@ -0,0 +1,40 @@
+import { Post } from "../entities/Post";
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+
+export class UpdateDeletedPosts1744802302723 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.manager.createQueryBuilder()
+            .update(Post)
+            .set({ isDeleted: true })
+            .where("id IN (:...ids)", { ids: targetPostIds })
+            .execute();    
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.manager.createQueryBuilder()
+            .update(Post)
+            .set({ isDeleted: false })
+            .where("id IN (:...ids)", { ids: targetPostIds })
+            .execute();
+    }
+
+}
+
+const targetPostIds = [
+    '1875806976611102749',
+    '1881537451149476037',
+    '1881627800299565408',
+    '1882262384200634748',
+    '1882624801074012358',
+    '1882702860233953636',
+    '1885213014464618763',
+    '1893501977658569064',
+    '1898207056508645842',
+    '1900394693659418728',
+    '1903283218281009486',
+    '1905106186762129677'
+];
