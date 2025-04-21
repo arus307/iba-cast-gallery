65124021b41f0d6545332ca7b78107625f430804
arus <57102101+arus307@users.noreply.github.com>
2025-04-21 12:15:27 +0000

    4/21のポストを追加

---
+++ packages/dao/src/migrations/1745236447444-insertPosts.ts
@@ -0,0 +1,49 @@
+import { Post } from "../entities/Post";
+import { Cast } from "../entities/Cast";
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InsertPosts1745236447444 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+            const casts : Cast[] = await queryRunner.manager.find("casts");
+    
+            // id指定になっているcastをインスタンスに設定
+            const posts : Post[] = basePosts.map((basePost)=>{
+                const post = new Post();
+                post.id = basePost.id;
+                post.postedAt = basePost.postedAt;
+                post.taggedCasts = basePost.taggedCastIds.map(castId => {
+                    const cast = casts.find(c => c.id === castId);
+                    if (!cast) {
+                        throw new Error(`Cast with id ${castId} not found`);
+                    }
+                    return cast;
+                });
+                post.isDeleted = false;
+                return post;
+            });
+    
+            await queryRunner.manager.save(posts);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+                const idsToDelete = basePosts.map(p => p.id);
+                await queryRunner.manager.createQueryBuilder()
+                    .delete()
+                    .from(Post)
+                    .where("id IN (:...ids)", { ids: idsToDelete })
+                    .execute();
+    }
+}
+
+const basePosts: {
+    id:string;
+    postedAt:string;
+    taggedCastIds: number[];
+}[] = [
+    {
+        id: "1914154099953877491",
+        postedAt: "2025-04-21T12:07:00+09:00",
+        taggedCastIds: [36, 4, 13, 25, 24,],
+    }
+];
