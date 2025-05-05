fdcfcfbcd8b7a5cffab2d4ecabfa9ad8205cff81
arus <57102101+arus307@users.noreply.github.com>
2025-05-05 09:11:52 +0000

    重複している部分をlayout.tsxに共通化

--- app/tweet-tagger/src/app/page.tsx
+++ app/tweet-tagger/src/app/page.tsx
@@ -21,14 +21,12 @@ export default async function Home() {
   }
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full">
-        <Link href="/posts">
-          <Typography variant="body2" color="primary">登録済みポスト一覧</Typography>
-        </Link>
-        <Typography>登録画面</Typography>
-        <TweetEditor />
-      </main>
-    </div>
+    <>
+      <Link href="/posts">
+        <Typography variant="body2" color="primary">登録済みポスト一覧</Typography>
+      </Link>
+      <Typography>登録画面</Typography>
+      <TweetEditor />
+    </>
   );
 }
--- app/tweet-tagger/src/app/posts/[postId]/edit/page.tsx
+++ app/tweet-tagger/src/app/posts/[postId]/edit/page.tsx
@@ -7,31 +7,29 @@ import NotAdmin from "app/client-component/NotAdmin";
 import Link from "next/link";
 
 export default async function Page(
-    { params }: { params: Promise<{ postId: string }> }
+  { params }: { params: Promise<{ postId: string }> }
 ) {
 
-    const session = await auth();
-    if (!session?.user) {
-        redirect('/api/auth/signin');
-    }
+  const session = await auth();
+  if (!session?.user) {
+    redirect('/api/auth/signin');
+  }
 
-    if (session.user.email !== process.env.ADMIN_EMAIL) {
-        return (
-            <NotAdmin />
-        );
-    }
-
-    const postId = await params.then(p => p.postId);
-
+  if (session.user.email !== process.env.ADMIN_EMAIL) {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full">
-                <Link href="/posts">
-                    <Typography variant="body2" color="primary">登録済みポスト一覧</Typography>
-                </Link>
-                <Typography>編集画面</Typography>
-                <Typography>ポストID: {postId}</Typography>
-            </main>
-        </div>
+      <NotAdmin />
     );
+  }
+
+  const postId = await params.then(p => p.postId);
+
+  return (
+    <>
+      <Link href="/posts">
+        <Typography variant="body2" color="primary">登録済みポスト一覧</Typography>
+      </Link>
+      <Typography>編集画面</Typography>
+      <Typography>ポストID: {postId}</Typography>
+    </>
+  );
 }
