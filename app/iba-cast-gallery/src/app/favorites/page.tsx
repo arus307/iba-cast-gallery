1735b48d71b374478d0c5548c2255de933a9b16b
arus <57102101+arus307@users.noreply.github.com>
2025-06-16 13:47:53 +0000

    お気に入りページを追加(仮)

--- app/iba-cast-gallery/src/app/favorites/page.tsx
+++ app/iba-cast-gallery/src/app/favorites/page.tsx
@@ -1,6 +1,26 @@
 "use client";
 
+import {useSession} from "next-auth/react";
+import {CircularProgress} from "@mui/material";
+
 export default function Favorites() {
+    
+    const { data: session, status } = useSession();
+
+    if(status === "loading") {
+        return (
+            <CircularProgress sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }} />
+        );
+    }
+
+
+    if(status !== "authenticated") {
+        return (
+            <div>
+                ログインが必要なページです。
+            </div>
+        );
+    }
 
     return (
         <div>
