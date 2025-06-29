a34accad9294ddc31261ed78a4c6689263be640f
arus <57102101+arus307@users.noreply.github.com>
2025-06-29 07:55:01 +0000

    Loaderを配置

---
+++ app/tweet-tagger/src/app/client-component/Loader.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import NextTopLoader from "nextjs-toploader";
+import { useTheme } from "@mui/material/styles";
+
+export default function Loader() {
+
+  const theme = useTheme();
+  const primaryColor = theme.palette.primary.main;
+
+  return (
+    <NextTopLoader
+      color={primaryColor}       // バーの色
+      initialPosition={0.08}
+      crawlSpeed={200}
+      height={3}
+      crawl={true}
+      showSpinner={true}
+      easing="ease"
+      speed={200}
+    />
+  );
+}
