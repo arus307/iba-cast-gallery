a131259cd4933413e26eb5cb808d6208e4f11d19
arus <57102101+arus307@users.noreply.github.com>
2025-07-06 14:31:57 +0000

    getActiveCasts()をServerAction化 + ドロワーにキャスト詳細へのリンク追加

--- app/iba-cast-gallery/src/app/client-components/MenuDrawer.tsx
+++ app/iba-cast-gallery/src/app/client-components/MenuDrawer.tsx
@@ -1,48 +1,78 @@
 "use client";
 
 import { useState } from "react";
-import { Drawer, Box, List, ListItem, ListItemButton, ListItemText, } from "@mui/material";
+import { Drawer, Box, List, ListItem, ListItemButton, ListItemText, Collapse, } from "@mui/material";
 import { ArrowBack } from "@mui/icons-material";
 import AboutDialog from "./AboutDialog";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import useSWR from "swr";
+import { getActiveCastsAction } from "../actions";
 
 interface MenuDrawerProps {
-    open: boolean;
-    setOpen: (open: boolean) => void;
+  open: boolean;
+  setOpen: (open: boolean) => void;
 }
 
 /**
  * メニュードロワー
  */
-const MenuDrawer = ({open, setOpen}:MenuDrawerProps) => {
-    const [openAboutDialog, setOpenAboutDialog] = useState(false);
-    const onClickAbout = ()=>{
-        setOpen(false);
-        setOpenAboutDialog(true);
-    };
+const MenuDrawer = ({ open, setOpen }: MenuDrawerProps) => {
+  const [openAboutDialog, setOpenAboutDialog] = useState(false);
+  const onClickAbout = () => {
+    setOpen(false);
+    setOpenAboutDialog(true);
+  };
 
-    return(
-        <>
-            <Drawer open={open} onClose={()=> setOpen(false)}>
-                <Box
-                    sx={{ width: 250 }}>
-                    <List>
-                        <ListItem>
-                            <ListItemButton onClick={() => setOpen(false)}>
-                                <ArrowBack />
-                                <ListItemText primary="戻る" />
-                            </ListItemButton>
-                        </ListItem>
-                        <ListItem>
-                            <ListItemButton onClick={onClickAbout}>
-                                <ListItemText primary="このサイトについて" />
-                            </ListItemButton>
-                        </ListItem>
-                    </List>
-                </Box>
-            </Drawer>
-            <AboutDialog open={openAboutDialog} setOpenDialog={setOpenAboutDialog} />
-        </>
-    );
+  const [isOpenCastList, setIsOpenCastList] = useState(false);
+  const toggleCastList = () => {
+    setIsOpenCastList(!isOpenCastList);
+  };
+
+  const {data: casts, error, isLoading } = useSWR('all-active-casts', getActiveCastsAction);
+
+  return (
+    <>
+      <Drawer open={open} onClose={() => setOpen(false)}>
+        <List sx={{ width: 250 }}>
+          <ListItem>
+            <ListItemButton onClick={() => setOpen(false)}>
+              <ArrowBack />
+              <ListItemText primary="戻る" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton onClick={onClickAbout}>
+              <ListItemText primary="このサイトについて" />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem>
+            <ListItemButton onClick={toggleCastList}>
+              <ListItemText primary="キャスト一覧" />
+              {isOpenCastList ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+          </ListItem>
+
+          <Collapse in={isOpenCastList}>
+            <List component="div" disablePadding>
+              {isLoading && <ListItemText primary="読み込み中..." />}
+              {error && <ListItemText primary="キャストの取得に失敗しました" />}
+              {
+                casts && casts.map((cast) => (
+                  <ListItem key={cast.id}>
+                    <ListItemButton href={`/casts/${cast.enName}`} sx={{ pl: 4 }}>
+                      <ListItemText primary={cast.name} />
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+            </List>
+          </Collapse>
+
+        </List>
+      </Drawer>
+      <AboutDialog open={openAboutDialog} setOpenDialog={setOpenAboutDialog} />
+    </>
+  );
 }
 
 export default MenuDrawer;
