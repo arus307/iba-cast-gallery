3a7aeb5bcc71ae104b1a3d98f3fb9922aae5dbfb
arus <57102101+arus307@users.noreply.github.com>
2025-08-11 10:29:48 +0000

    デバッグ用にログ出力追加

--- app/tweet-tagger/src/app/page.tsx
+++ app/tweet-tagger/src/app/page.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 export default async function Home() {
 
   const session = await auth();
+  console.log("session:", session);
   if (!session?.user) {
     redirect('/api/auth/signin');
   }
