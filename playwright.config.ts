3dc116c73dad5fab8632d098c09df74c6df31fff
arus <57102101+arus307@users.noreply.github.com>
2025-08-11 09:38:38 +0000

    Revert "AUTH_URLを設定"
    This reverts commit 4645a334421977cdaf3dbc6a2fb0d2a9547b4487.

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,9 +9,8 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
-  timeout: 10 * 1000,
   use: {
-    trace: 'on',
+    trace: 'on-first-retry',
   },
 
   projects: [
