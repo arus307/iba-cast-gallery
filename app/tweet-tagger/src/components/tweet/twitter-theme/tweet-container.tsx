b5449971a67c39eb06d5884193312f4a7b20e79e
arus <57102101+arus307@users.noreply.github.com>
2025-08-11 13:44:37 +0000

    dataTestIdをnull許容に

--- app/tweet-tagger/src/components/tweet/twitter-theme/tweet-container.tsx
+++ app/tweet-tagger/src/components/tweet/twitter-theme/tweet-container.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx'
 import s from './tweet-container.module.css'
 import './theme.css'
 
-type Props = { className?: string; children: ReactNode, dataTestId:string; }
+type Props = { className?: string; children: ReactNode, dataTestId?:string; }
 
 export const TweetContainer = ({ className, children, dataTestId }: Props) => (
   <div className={clsx('react-tweet-theme', s.root, className)}>
