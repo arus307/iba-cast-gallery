4b9645936dcf4d992d9c2e44d3d392cef3a8d287
arus <57102101+arus307@users.noreply.github.com>
2025-09-25 13:54:05 +0000

    ぎゃらりー→ぎゃらりぃに表現変更

--- app/iba-cast-gallery/src/app/client-components/AppBar.tsx
+++ app/iba-cast-gallery/src/app/client-components/AppBar.tsx
@@ -34,7 +34,7 @@ const AppBar = () => {
                             py: 2,
                             px: 1,
                         }}>
-                            IBAぎゃらりー
+                            IBAぎゃらりぃ
                         </Typography>
                     </Link>
                 </Toolbar>
