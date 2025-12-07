1bd52420e427f636f91fdbbcb89856e5cbfac2cc
arus <57102101+arus307@users.noreply.github.com>
2025-12-07 11:41:24 +0000

    MUIのアップデートに伴いGrid2->Grid

--- app/iba-cast-gallery/src/app/client-components/TweetFilter.tsx
+++ app/iba-cast-gallery/src/app/client-components/TweetFilter.tsx
@@ -1,32 +1,45 @@
 "use client";
 
-import Grid2 from "@mui/material/Grid2";
+import { Grid } from "@mui/material";
 import CastSelect from "components/CastSelect";
 import { useState } from "react";
 import Tweets from "components/Tweets";
 import { CastDto } from "@iba-cast-gallery/types";
 import dayjs from "dayjs";
 
-export default function TweetFilter ({posts, casts}:{posts:JoinedPost[], casts:CastDto[]}) {
-
-    const [selectedCast, setSelectedCast] = useState<CastDto | null>(null);
-    const displayPosts = posts.filter((post) => {
-        if (selectedCast) {
-            return post.taggedCasts.some((cast) => cast.cast.id === selectedCast.id);
-        } else {
-            return true;
-        }
-    }).sort((a, b) => {
-        const diff = dayjs(b.postedAt).diff(dayjs(a.postedAt));
-        return diff > 0 ? 1 : diff < 0 ? -1 : 0;
+export default function TweetFilter({
+  posts,
+  casts,
+}: {
+  posts: JoinedPost[];
+  casts: CastDto[];
+}) {
+  const [selectedCast, setSelectedCast] = useState<CastDto | null>(null);
+  const displayPosts = posts
+    .filter((post) => {
+      if (selectedCast) {
+        return post.taggedCasts.some(
+          (cast) => cast.cast.id === selectedCast.id
+        );
+      } else {
+        return true;
+      }
+    })
+    .sort((a, b) => {
+      const diff = dayjs(b.postedAt).diff(dayjs(a.postedAt));
+      return diff > 0 ? 1 : diff < 0 ? -1 : 0;
     });
 
-    return (
-    <Grid2 container spacing={2} className="w-full">
-        <Grid2 size={12}>
-            <CastSelect casts={casts} selectedCast={selectedCast} setSelectedCast={setSelectedCast} />
-        </Grid2>
-        <Tweets joinedPosts={displayPosts} />
-    </Grid2>
-    );
-};
+  return (
+    <Grid container spacing={2} className="w-full">
+      <Grid size={12}>
+        <CastSelect
+          casts={casts}
+          selectedCast={selectedCast}
+          setSelectedCast={setSelectedCast}
+        />
+      </Grid>
+      <Tweets joinedPosts={displayPosts} />
+    </Grid>
+  );
+}
--- app/iba-cast-gallery/src/components/Tweets.tsx
+++ app/iba-cast-gallery/src/components/Tweets.tsx
@@ -1,16 +1,19 @@
 import TweetCard from "components/TweetCard";
-import Grid2 from "@mui/material/Grid2";
+import { Grid } from "@mui/material";
 import { PostWithCastsDto } from "@iba-cast-gallery/types";
 
-export default function Tweets ({joinedPosts}:{joinedPosts: PostWithCastsDto[]}) {
-
+export default function Tweets({
+  joinedPosts,
+}: {
+  joinedPosts: PostWithCastsDto[];
+}) {
   return (
-    <Grid2 container spacing={2} className="w-full">
-        {joinedPosts.map((tweet) => (
-            <Grid2 key={tweet.id} size={{xs:12, md:6, lg:4, xl:3}}>
-                <TweetCard tweet={tweet} />
-            </Grid2>
-        ))}
-    </Grid2>
+    <Grid container spacing={2} className="w-full">
+      {joinedPosts.map((tweet) => (
+        <Grid key={tweet.id} size={{ xs: 12, md: 6, lg: 4, xl: 3 }}>
+          <TweetCard tweet={tweet} />
+        </Grid>
+      ))}
+    </Grid>
   );
-};
+}
